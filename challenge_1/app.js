41ae08e835b2eea94d9353fa7d7edee7786a4a68
Elizabeth Li <sincerelylibby@gmail.com>
2018-05-21 14:36:41 -0700

    Implemented toggling between turns and X/O.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,29 @@
 const app = () => {
+  let turns = 'playerX';
   const board = document.getElementById('board');
   const cells = board.getElementsByTagName('td');
-  for (let i = 0; i < cells.length; i += 1) {
-    cells[i].onclick = () => {
-      cells[i].innerHTML = 'x';
+
+  const addXO = (event) => {
+    if (turns === 'playerX') {
+      event.target.innerHTML = 'X';
+    } else {
+      event.target.innerHTML = 'O';
+    }
+    switchTurns();
+  }
+
+  const switchTurns = () => {
+    if (turns === 'playerX') {
+      turns = 'playerO';
+    } else {
+      turns = 'playerX';
     }
   }
+
+  for (let i = 0; i < cells.length; i += 1) {
+    cells[i].onclick = this.addXO;
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', app);
