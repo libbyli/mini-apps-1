6daf600b384e1b53cde4c8c8ec87f82f4b754bf3
Elizabeth Li <sincerelylibby@gmail.com>
2018-05-21 13:57:45 -0700

    Added click handlers to all table cells and added DOMcontentloaded.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
+const app = () => {
+  const board = document.getElementById('board');
+  const cells = board.getElementsByTagName('td');
+  for (let i = 0; i < cells.length; i += 1) {
+    cells[i].onclick = () => {
+      cells[i].innerHTML = 'x';
+    }
+  }
+}
 
-
+document.addEventListener('DOMContentLoaded', app);
