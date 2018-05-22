8f1af2d85ed4e4fdf40e3e5ce068ae37831f0516
Elizabeth Li <sincerelylibby@gmail.com>
2018-05-22 15:14:17 -0700

    Set up server and client, working on post request.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,45 @@
+const app = {
 
+  init() {
+    $('form').on('submit', function(e){
+      e.preventDefault();
+      let input = {
+        text: $('textarea').val()
+      }
+      app.send(input);
+    });
+  },
 
+  send(input) {
+    $.ajax({
+      url: '/',
+      type: 'POST',
+      data: JSON.stringify(input),
+      contentType: 'application/json',
+      success: (data) => {
+        console.log('data------> ', data);
+      },
+      error(error) {
+        console.error('error: ', error)
+      }
+    })
+  },
+
+  fetch() {
+    $.ajax({
+      url: '/',
+      type: 'GET',
+      data: {},
+      success: (data) => {
+        console.log('data: ', data);
+      },
+      error: (data) => {
+        console.error('error: ', error);
+      }
+    })
+  }
+}
+
+$(document).ready(function() {
+  app.init();
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.listen(27016);
+
+app.use(express.static('client'));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('get request')
+});
+
+app.post('/', (req, res) => {
+  let output = req.body.text;
+  JSON.stringify(output);
 
 
+  console.log(output);
+  res.send(output);
+});
+
+const formatData = (obj) => {
+  let result = [];
+  let keys = Object.keys(obj);
+  
+  console.log(keys);
+}
