2f2c6990309223d1939e32121385f9898cd4ac64
Elizabeth Li <sincerelylibby@gmail.com>
2018-05-22 19:38:38 -0700

    Refactored code and added in unique identifier..

--- challenge_2/helper.js
+++ challenge_2/helper.js
@@ -1,34 +1,37 @@
 exports.formatData = obj => {
   obj = JSON.parse(obj);
   const result = [];
-
-  const keys = [];
-  for (const key in obj) {
-    if (key !== 'children') {
-      keys.push(key);
+  const keys = [1];
+  const objKeys = Object.keys(obj);
+  for (let i = 0; i < objKeys.length; i += 1) {
+    if (objKeys[i] !== 'children') {
+      keys.push(objKeys[i]);
     }
   }
 
-  const values = [];
-  for (const key in obj) {
-    if (key !== 'children') {
-      values.push(obj[key]);
+  const values = [2];
+  for (let i = 0; i < objKeys.length; i += 1) {
+    if (objKeys[i] !== 'children') {
+      values.push(obj[objKeys[i]]);
     }
   }
 
   result.push(keys, values);
 
+  let counter = 3;
   const formatChildren = child => {
     if (child.children === undefined) {
       return;
     }
-    const childValues = [];
-    for (const key in child) {
-      if (key !== 'children') {
-        childValues.push(child[key]);
+    const childValues = [counter];
+    const childKeys = Object.keys(child);
+    for (let i = 0; i < childKeys.length; i += 1) {
+      if (childKeys[i] !== 'children') {
+        childValues.push(child[childKeys[i]]);
       }
     }
     result.push(childValues);
+    counter += 1;
     child.children.forEach((child) => formatChildren(child));
   };
 
